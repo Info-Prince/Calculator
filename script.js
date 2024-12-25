488e1f6a69ac74a2660db66c3c6f005ffdef5d42
Prince Kumar <princeinfo053@gmail.com>
2024-12-25 07:21:59 +0000

    dot handle

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ let result = "";
 // console.log(inputVal);
 
 document.addEventListener('keydown', (event) => {
-    const operators = ['+', '-', '*', '/'];
-    
+    const operators = ['+', '-', '*', '/', '.'];
+    let inputArr = [];
+    inputArr.push(event.key);
     event.preventDefault();
+
 
     if(event.key === "Enter") {
         let inputData = inputVal.value;
@@ -20,15 +22,25 @@ document.addEventListener('keydown', (event) => {
         inputVal.value = (Number(result).toFixed(2))
     }
     else if (operators.includes(event.key)){
-        let lastElem = inputVal.value.slice(-1);
         
+        if (event.key === '.') {
+            let segments = inputVal.value.split(/[\+\-\*\/]/); // Split by operators
+            let lastSegment = segments[segments.length - 1];
+            if (lastSegment.includes('.')) return; // Ignore if the last number already has a dot
+        }
+        
+        // Handle last operator
+        let lastElem = inputVal.value.slice(-1);
         if(operators.includes(lastElem)) {
+            if(lastElem == '.')
+                inputVal.value = inputVal.value.slice(0,-1) + lastElem;
+            else
             inputVal.value = inputVal.value.slice(0,-1) + event.key;
         }
         else 
         inputVal.value += event.key;
     }
-    else if (!isNaN(event.key) || event.key === '.') {
+    else if (!isNaN(event.key)) {
         inputVal.value += event.key;
     }
     else if (event.key === 'Backspace') {
@@ -60,15 +72,21 @@ buttons.forEach((button) => {
             inputVal.value = (Number(result).toFixed(2))
         }
         else {
-            const operators = ['+', '-' ,'/' ,'*'];
+            const operators = ['+', '-' ,'/' ,'*', '.'];
             let lastElem = inputVal.value.slice(-1);
             
+            if(currelem === '.') {
+                let segments = inputVal.value.split(/[\+\-\*\/]/); // Split by operators
+                let lastSegment = segments[segments.length - 1];
+                if (lastSegment.includes('.')) return; // Ignore if the last number already has a dot
+            }
+
             if(operators.includes(currelem) && operators.includes(lastElem)) {
                 inputVal.value = inputVal.value.slice(0, -1) + currelem;
             } 
             else {
                 inputVal.value += currelem;
-                console.log(inputVal.value);    
+                // console.log(inputVal.value);    
             }
         }    
         
